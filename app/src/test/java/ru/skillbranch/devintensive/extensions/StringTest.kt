b019bd619e791054e28591590029b0cc1078223d
Igor K <russdreamer@gmail.com>
2019-07-04 11:17:18 +0200

    updated truncate method as bot tests fixed

--- app/src/test/java/ru/skillbranch/devintensive/extensions/StringTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/extensions/StringTest.kt
@@ -34,18 +34,23 @@ class StringTest {
     @Test
     fun truncateTest() {
         /* skillBranch tests */
-        assertEquals("Bender Bending Ro...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
-        assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
+        assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
+        assertEquals("Bender Bending...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
         assertEquals("A", "A     ".truncate(3))
 
         /* additional tests */
-        assertEquals("too lo...", "   too long line with lots of spaces before".truncate(5))
+        assertEquals("too lo...", "   too long line with lots of spaces before".truncate(6))
         assertEquals("too short", "too short".truncate(20))
-        assertEquals("12345", "12345".truncate(4))
-        assertEquals("1234...", "12345".truncate(3))
-        assertEquals("12345", "12345  ".truncate(4))
-        assertEquals("tab", "tab    ".truncate(4))
-        assertEquals("dots......", "dots... a lot".truncate(6))
+        assertEquals("12345", "12345".truncate(5))
+        assertEquals("1234...", "12345".truncate(4))
+        assertEquals("12345", "12345  ".truncate(5))
+        assertEquals("tab", "tab    ".truncate(5))
+        assertEquals("dots......", "dots... a lot".truncate(7))
         assertEquals("abc", "abc    ".truncate(15))
+        assertEquals("123456...", "123456789".truncate(6))
+        assertEquals("123456789", "123456789".truncate(9))
+        assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
+        assertEquals("Bender Bending...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
+        assertEquals("1", "1     ".truncate(3))
     }
 }
