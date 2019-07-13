cec53deac1d650e7dfbd851894ae9bb71375000d
Igor K <russdreamer@gmail.com>
2019-07-13 17:39:58 +0200

    improve positive and negative test by replace on UI only

--- app/src/androidTest/java/ru/skillbranch/devintensive/Task2.kt
+++ app/src/androidTest/java/ru/skillbranch/devintensive/Task2.kt
@@ -9,7 +9,6 @@ import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import ru.skillbranch.devintensive.models.Bender
 
 @RunWith(AndroidJUnit4::class)
 class Task2 {
@@ -19,47 +18,55 @@ class Task2 {
 
     @Test
     fun listenAnswerPositiveTest1(){
-        val bender = Bender()
-        assertEquals("Как меня зовут?", bender.question.question)
-        var response = bender.listenAnswer("bender")
-        assertEquals("Отлично - ты справился\nНазови мою профессию?", response.first)
-        response = bender.listenAnswer("bender")
-        assertEquals("Отлично - ты справился\nИз чего я сделан?", response.first)
-        response = bender.listenAnswer("iron")
-        assertEquals("Отлично - ты справился\nКогда меня создали?", response.first)
-        response = bender.listenAnswer("2993")
-        assertEquals("Отлично - ты справился\nМой серийный номер?", response.first)
-        response = bender.listenAnswer("2716057")
-        assertEquals("Отлично - ты справился\nНа этом все, вопросов больше нет", response.first)
+        assertEquals("Как меня зовут?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Bender"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nНазови мою профессию?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("bender"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nИз чего я сделан?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("iron"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nКогда меня создали?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("2993"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nМой серийный номер?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("2716057"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nНа этом все, вопросов больше нет", rule.activity.textTxt.text)
     }
 
     @Test
     fun listenAnswerPositiveTest2(){
-        val bender = Bender()
-        assertEquals("Как меня зовут?", bender.question.question)
-        var response = bender.listenAnswer("bender")
-        assertEquals("Отлично - ты справился\nНазови мою профессию?", response.first)
-        response = bender.listenAnswer("сгибальщик")
-        assertEquals("Отлично - ты справился\nИз чего я сделан?", response.first)
-        response = bender.listenAnswer("дерево")
-        assertEquals("Отлично - ты справился\nКогда меня создали?", response.first)
-        response = bender.listenAnswer("2993")
-        assertEquals("Отлично - ты справился\nМой серийный номер?", response.first)
-        response = bender.listenAnswer("2716057")
-        assertEquals("Отлично - ты справился\nНа этом все, вопросов больше нет", response.first)
+        assertEquals("Как меня зовут?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Бендер"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nНазови мою профессию?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("сгибальщик"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nИз чего я сделан?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("дерево"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nКогда меня создали?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("2993"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nМой серийный номер?", rule.activity.textTxt.text)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("2716057"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Отлично - ты справился\nНа этом все, вопросов больше нет", rule.activity.textTxt.text)
     }
 
     @Test
     fun saveStateWhenRotateTest(){
         assertEquals("Как меня зовут?", rule.activity.textTxt.text)
-        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.typeText("Bender"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Bender"))
         Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
         assertEquals("Отлично - ты справился\nНазови мою профессию?", rule.activity.textTxt.text)
         rotateScreen(rule.activity, true)
         assertEquals("Назови мою профессию?", rule.activity.textTxt.text)
         rotateScreen(rule.activity, false)
         assertEquals("Назови мою профессию?", rule.activity.textTxt.text)
-        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.typeText("bender"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("bender"))
         Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
         assertEquals("Отлично - ты справился\nИз чего я сделан?", rule.activity.textTxt.text)
     }
