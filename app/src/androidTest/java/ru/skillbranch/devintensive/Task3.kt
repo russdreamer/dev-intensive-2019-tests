e9f035b22f1246bd3453ab533358283d64b9bf47
Igor K <russdreamer@gmail.com>
2019-07-15 16:46:21 +0200

    add color filter check test

--- app/src/androidTest/java/ru/skillbranch/devintensive/Task3.kt
+++ app/src/androidTest/java/ru/skillbranch/devintensive/Task3.kt
@@ -1,5 +1,8 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.matcher.ViewMatchers
@@ -142,5 +145,42 @@ class Task3 {
         Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.typeText("Ender"))
         Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
         assertEquals("Это неправильный ответ. Давай все по новой\nКак меня зовут?", rule.activity.textTxt.text)
+    }
+
+    @Test
+    fun saveStateWhenRotateTest(){
+        assertEquals("Как меня зовут?", rule.activity.textTxt.text)
+        val normalColorFilter = PorterDuffColorFilter(Color.rgb(255, 255, 255), PorterDuff.Mode.MULTIPLY)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
+
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Tinder"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Это неправильный ответ\nКак меня зовут?", rule.activity.textTxt.text)
+        val warnColorFilter = PorterDuffColorFilter(Color.rgb(255, 120, 0), PorterDuff.Mode.MULTIPLY)
+        assertEquals(warnColorFilter, rule.activity.benderImage.colorFilter)
+
+        rotateScreen(rule.activity, true)
+        assertEquals("Как меня зовут?", rule.activity.textTxt.text)
+        assertEquals(warnColorFilter, rule.activity.benderImage.colorFilter)
+
+        rotateScreen(rule.activity, false)
+        assertEquals("Как меня зовут?", rule.activity.textTxt.text)
+        assertEquals(warnColorFilter, rule.activity.benderImage.colorFilter)
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Blender"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Это неправильный ответ\nКак меня зовут?", rule.activity.textTxt.text)
+        val dangerColorFilter = PorterDuffColorFilter(Color.rgb(255, 60, 60), PorterDuff.Mode.MULTIPLY)
+        assertEquals(dangerColorFilter, rule.activity.benderImage.colorFilter)
+
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Espander"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Это неправильный ответ\nКак меня зовут?", rule.activity.textTxt.text)
+        val critColorFilter = PorterDuffColorFilter(Color.rgb(255, 0, 0), PorterDuff.Mode.MULTIPLY)
+        assertEquals(critColorFilter, rule.activity.benderImage.colorFilter)
+
+        Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Labrador"))
+        Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
+        assertEquals("Это неправильный ответ. Давай все по новой\nКак меня зовут?", rule.activity.textTxt.text)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
     }
 }
