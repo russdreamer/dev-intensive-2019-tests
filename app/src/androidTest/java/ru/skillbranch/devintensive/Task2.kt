57dd4c69c4f12a6fee5867f816c83b70b413e96d
Igor K <russdreamer@gmail.com>
2019-07-15 16:52:59 +0200

    add color filter check test

--- app/src/androidTest/java/ru/skillbranch/devintensive/Task2.kt
+++ app/src/androidTest/java/ru/skillbranch/devintensive/Task2.kt
@@ -1,14 +1,19 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
+
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import ru.skillbranch.devintensive.models.Bender
 
 @RunWith(AndroidJUnit4::class)
 class Task2 {
@@ -59,16 +64,23 @@ class Task2 {
     @Test
     fun saveStateWhenRotateTest(){
         assertEquals("Как меня зовут?", rule.activity.textTxt.text)
+        val normalColorFilter = PorterDuffColorFilter(Color.rgb(255, 255, 255), PorterDuff.Mode.MULTIPLY)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
+
         Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("Bender"))
         Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
         assertEquals("Отлично - ты справился\nНазови мою профессию?", rule.activity.textTxt.text)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
         rotateScreen(rule.activity, true)
         assertEquals("Назови мою профессию?", rule.activity.textTxt.text)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
         rotateScreen(rule.activity, false)
         assertEquals("Назови мою профессию?", rule.activity.textTxt.text)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
         Espresso.onView(ViewMatchers.withId(rule.activity.messageEt.id)).perform(ViewActions.replaceText("bender"))
         Espresso.onView(ViewMatchers.withId(rule.activity.sendBtn.id)).perform(ViewActions.click())
         assertEquals("Отлично - ты справился\nИз чего я сделан?", rule.activity.textTxt.text)
+        assertEquals(normalColorFilter, rule.activity.benderImage.colorFilter)
     }
 
     @Test
